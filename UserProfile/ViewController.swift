3ffba7283b280f518f2f1f9585553be1b3195ef4
rshad <mohamed88rshad@gmail.com>
2025-04-12 14:50:03 +0200

    Initial Commit

---
+++ UserProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserProfile
+//
+//  Created by mohamed rshad on 12/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
